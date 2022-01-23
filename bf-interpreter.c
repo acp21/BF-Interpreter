77637f865df6247ac9e1d288e64c489e5a4b4d07
Adam Pohl <acpohl21@gmail.com>
2022-01-23 01:03:30 -0500

    Starting bracket development

--- bf-interpreter.c
+++ bf-interpreter.c
@@ -1,7 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "bf-interpreter.h"
+
+#define ARR_SIZE 30000
+
+int arr[ARR_SIZE];
+int ptr = 0;
 
 
 int main(int argc, char **argv){
-    printf("Hello World\n");
+    if(argc < 2){
+        printf("No path to BF file.");
+        return 1;
+    }
+    if(argc > 3){
+        printf("Too many arguments passed.");
+        return 1;
+    }
+
+    FILE *bf;
+    FILE *in;
+    if(argc == 3){
+        in = fopen(argv[2], "r");
+    }
+
+    bf = fopen(argv[1], "r");
+    char c;
+    c = fgetc(bf);
+    while(c != EOF){
+        if(c == '+'){
+            arr[ptr]++;
+        }
+        else if(c == '-'){
+            arr[ptr]--;
+        }
+        else if(c == '>'){
+            ptr++;
+        }
+        else if(c == '<'){
+            ptr--;
+        }
+        else if(c == '.'){
+            printf("%c", arr[ptr]);
+        }
+        else if(c == ','){
+            arr[ptr] = fgetc(in);
+        }
+        else if(c == '['){
+            if(arr[ptr] == 0){
+                while(1){
+                    if(fgetc(bf) == ']') break;
+                }
+            }
+        }
+        else if(c == ']'){
+            if(arr[ptr] != 0){
+
+            }
+        }
+        c = fgetc(bf);
+    }
+
+    printf("\n");
+    fclose(bf);
+    return 0;
 }
---
+++ bf-interpreter.h
@@ -0,0 +1,11 @@
+#ifndef BF_INTERPRETER_H
+#define BF_INTERPRETER_H
+
+#include <aio.h>
+
+typedef struct brackets{
+    off_t first;
+    off_t second;
+} brackets_t;
+
+#endif
